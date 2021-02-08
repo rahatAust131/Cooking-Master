3a2ac409dbe138885321511ab19681b07758aee8
Rahat Chowdhury <rczisan@gmail.com>
2021-02-08 20:17:40 +0600

    added alert or error message

--- js/script.js
+++ js/script.js
@@ -6,47 +6,54 @@ const searchBtn = document.getElementById('search-btn');
 searchBtn.addEventListener('click', () => {
     // Calling the fetchMealsData function
     fetchMealsData();
-    
-    // Clear the previous searched text
-    input.value = '';
-
     // Clear the previous Meals Info
     document.getElementById('meal-container').innerHTML = '';
+    
 });
 
 // A function to fetch Meals Data from API
 function fetchMealsData() {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
-        .then(res => res.json())
+        .then(res => res.json()).
         // Calling the showMealsData function
-        .then(data => showMealsData(data.meals));
+        then(data => showMealsData(data.meals))
 };
 
 // A function to show Meals Data from API
 const showMealsData = meals => {
-    // for getting each meal item, iterating through the meals
-    meals.forEach(mealItem => {
-        const mealContainer = document.getElementById('meal-container');
-        const mealInfoDiv = document.createElement('div');
-        mealInfoDiv.className = 'meal-info';
-        const mealInfo = `
-            <div>
-                <img src= ${mealItem.strMealThumb} />
-            </div>
-            <div>
-                <h1 id="meal-name" >${mealItem.strMeal}</h1>
-            </div>
-        `;
-        mealInfoDiv.innerHTML = mealInfo;
-        mealContainer.appendChild(mealInfoDiv);
-        // Calling the fetchData function to get the selected Meal Ingredients 
-        mealInfoDiv.onclick = () => {
-            fetchIngredientsData(mealItem.strMeal);
-
-            // Clear the previous Meals' Ingredients Info 
-            document.getElementById('ingredient-container').innerHTML = '';
-        };
-    })    
+    if(meals == null) {
+        alert(`
+        Sorry!
+        No Meal Found For : ${input.value} :(
+        Please Try Again With Another Valid Meal Name.
+        Thank You`);
+        
+    } else {
+        // for getting each meal item, iterating through the meals
+        meals.forEach(mealItem => {
+            const mealContainer = document.getElementById('meal-container');
+            const mealInfoDiv = document.createElement('div');
+            mealInfoDiv.className = 'meal-info';
+            const mealInfo = `
+                <div>
+                    <img src= ${mealItem.strMealThumb} />
+                </div>
+                <div>
+                    <h1 id="meal-name" >${mealItem.strMeal}</h1>
+                </div>
+            `;
+            mealInfoDiv.innerHTML = mealInfo;
+            mealContainer.appendChild(mealInfoDiv);        
+            // Calling the fetchData function to get the selected Meal Ingredients 
+            mealInfoDiv.onclick = () => {
+                fetchIngredientsData(mealItem.strMeal);
+                // Clear the previous Meals' Ingredients Info 
+                document.getElementById('ingredient-container').innerHTML = '';
+            };
+        })
+    } 
+    // Clear the previous searched text
+    input.value = ''; 
 }
 
 // A function to fetch Meals' Ingredients Data from API
@@ -61,7 +68,6 @@ const fetchIngredientsData = particularMeal => {
 const showIngredientsData = mealInfo => {
     // targeting static div
     const ingredientsDiv = document.getElementById('ingredient-container');
-
     // dynamic html
     const ingredientsInfo = `
     <div class="ingredients-top">
@@ -80,13 +86,10 @@ const showIngredientsData = mealInfo => {
             <li class="list-item"><i class="fas fa-check-square"></i> ${mealInfo.strIngredient7}</li>
         </ul>            
     </div>`;
-
     // creating dynamic div
     const ingredientsInfoDiv = document.createElement('div');
-
     // assigning html to div
     ingredientsInfoDiv.innerHTML = ingredientsInfo;
-
     // appending dynamic div to static div
     ingredientsDiv.appendChild(ingredientsInfoDiv);
 }
