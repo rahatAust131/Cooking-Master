8b7d6aa9db648bd7de3de6a6364b423743eb151a
Rahat Chowdhury <rczisan@gmail.com>
2021-02-08 12:52:25 +0600

    added main part

--- js/script.js
+++ js/script.js
@@ -1,56 +1,68 @@
-// fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f='${input}'`)
+// Targeting Search Input field
+const input = document.getElementById("search-input");
 
-// targeting input field
-const inputField = document.getElementById("search-input");
-const input = inputField.value;
+// Search-Button interaction
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', () => {
+    fetchMealsData();
+});
 
-// A function to fetch data from API
+// A function to fetch Meals Data from API
 function fetchMealsData() {
-    console.log('hi there');
-    fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-    .then(res => res.json())
-    .then(data => showMealsData(data.categories));
+    console.log(input.value);
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+        .then(res => res.json())
+        .then(data => showMealsData(data.meals));
 };
 
-// A function to show data from API
-const showMealsData = meal => {
-    meal.forEach(mealItem => {
+// A function to show Meals Data from API
+const showMealsData = meals => {
+    // for getting each meal item, iterating through the meals
+    meals.forEach(mealItem => {
         const mealContainer = document.getElementById('meal-container');
-        const mealDiv = document.createElement('div');
-        mealDiv.className = 'meal-info';
-        mealDiv.onclick = `showIngredients('${mealItem})`;
+        const mealInfoDiv = document.createElement('div');
+        mealInfoDiv.className = 'meal-info';
         const mealInfo = `
-        <img src= ${mealItem.strCategoryThumb} >
-        <h1>${mealItem.strCategory}</h1>
-        <p>Details</p>`;
-        mealDiv.innerHTML = mealInfo;
-        mealContainer.appendChild(mealDiv);
+            <img src= ${mealItem.strMealThumb} />
+            <h1>${mealItem.strMeal}</h1>
+        `;
+        mealInfoDiv.innerHTML = mealInfo;
+        mealContainer.appendChild(mealInfoDiv);
+        // call the fetchData function to get the ingredients of selected meal 
+        mealInfoDiv.onclick = () => {
+            fetchIngredientsData(mealItem.strMeal);
+        };
     })
 }
 
-fetchMealsData();
-
-// Function to 
-const fetchIngredientsData = () => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`)
-    .then(res => res.json())
-    .then(data => showIngredients(data.meals[0]));
-    console.log(data.meals[0]);
-} 
-
-const showIngredients = meal => {
-    const ingredientDiv = document.getElementById('ingredient-container');
-        const ingredientInfo = `
-            <img src= ${meal.strMealThumb} >
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-            </ul>
-        `;
-    ingredientDiv.appendChild(ingredientInfo);
+// A function to fetch Meals' Ingredients Data from API
+const fetchIngredientsData = particularMeal => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${particularMeal}`)
+        .then(res => res.json())
+        .then(data => showIngredientsData(data.meals[0]));
 }
 
-fetchIngredientsData();
+// A function to show Meals' Ingredients Data from API
+const showIngredientsData = mealInfo => {
+    const ingredientsDiv = document.getElementById('ingredient-container');
+    const ingredientsInfo = `
+    <div class="ingredients ingredients-top">
+        <img id="ingredient-img" src= ${mealInfo.strMealThumb} alt="" />
+    </div>
+    <div class="ingredients ingredients-bottom">
+        <h1 class="heading h1">${mealInfo.strMeal}</h1>
+        <h3 class="heading h2">Ingredients</h3>
+        <ul id="ingredients-list">
+            <li class="list-item"><i class="fas fa-check-square"></i> ${mealInfo.strIngredient1}</li>
+            <li class="list-item"><i class="fas fa-check-square"></i> ${mealInfo.strIngredient2}</li>
+            <li class="list-item"><i class="fas fa-check-square"></i> ${mealInfo.strIngredient3}</li>
+            <li class="list-item"><i class="fas fa-check-square"></i> ${mealInfo.strIngredient4}</li>
+            <li class="list-item"><i class="fas fa-check-square"></i> ${mealInfo.strIngredient5}</li>
+            <li class="list-item"><i class="fas fa-check-square"></i> ${mealInfo.strIngredient6}</li>
+            <li class="list-item"><i class="fas fa-check-square"></i> ${mealInfo.strIngredient7}</li>
+        </ul>            
+    </div>`;
+    const ingredientsInfoDiv = document.createElement('div');
+    ingredientsInfoDiv.innerHTML = ingredientsInfo;
+    ingredientsDiv.appendChild(ingredientsInfoDiv);
+}
